379371897d74060550f8997d099244a2ec34ca43
Gavin Beatty <gavinbeatty@gmail.com>
2010-09-01 21:42:09 +0200

    rejig the MSVC cc detection

--- src/plat/cc.h
+++ src/plat/cc.h
@@ -15,15 +15,29 @@
 #   define PLAT_CC_MWERKS 1
 #elif defined(_MSC_VER)
 #   define PLAT_CC_MSVC 1
-#   if _MSC_VER >= 1400
+#   if _MSC_VER >= 1600
+#       define PLAT_CC_MSVC_2010 1
+#       define PLAT_CC_MSVC_V10 1
+#   elif _MSC_VER >= 1500
+#       define PLAT_CC_MSVC_2008
+#       define PLAT_CC_MSVC_V9 1
+#   elif _MSC_VER >= 1400
 #       define PLAT_CC_MSVC_2005 1
+#       define PLAT_CC_MSVC_V8 1
 #   elif _MSC_VER >= 1310
 #       define PLAT_CC_MSVC_2003 1
-#   elif _MSC_VER > 1300
+#       define PLAT_CC_MSVC_V7_1 1
+#   elif _MSC_VER >= 1300
 #       define PLAT_CC_MSVC_2002 1
-#   endif /* #if _MSC_VER >= 1400 */
+#       define PLAT_CC_MSVC_V7 1
+#   elif _MSC_VER >= 1200
+#       define PLAT_CC_MSVC_V6 1
+#   endif /* #if _MSC_VER >= 1600 */
 #   if _MSC_VER >= 1300
 #       define PLAT_CC_MSVC_NET 1
+#   endif
+#   if defined(_MSC_FULL_VER)
+#       define PLAT_CC_MSVC_VER _MSC_FULL_VER
 #   endif
 #elif defined(__BORLANDC__)
 #   define PLAT_CC_BOR 1
